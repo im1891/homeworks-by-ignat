92ba59d3ea7f6a9cb4b528a799964abe9a041ba3
Igor Muratov <im1891@gmail.com>
2022-09-19 22:07:56 +0300

    Completed HW5.

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './Header.module.css'
+
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openHandler = () => {
+        console.log(isOpen)
+        setIsOpen(!isOpen)
+    }
+
+    const closeHandler = () => {
+        console.log(isOpen)
+        isOpen && setTimeout(openHandler, 100)
+    }
+
+    return (<div className={s.headerWrapper}>
+
+
+            <div onMouseLeave={closeHandler} className={`${s.linksBlock} ${isOpen ? s.open : s.close}`}>
+
+                <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+                <NavLink to={PATH.JUNIOR}>junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS}>junior+</NavLink>
+
+            </div>
+
+            <div className={`${s.button} ${isOpen ? s.buttonHidden : ''}`} onMouseOver={openHandler}></div>
+
 
         </div>
+
     )
 }
 
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Error404() {
     return (
         <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div style={{color: 'red', fontSize: '100px', textAlign: 'center'}}>404</div>
+            <div style={{fontSize: '30px', textAlign: 'center'}}>Page not found!</div>
+            <div style={{fontSize: '20px', textAlign: 'center'}}>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
         </div>
     )
 }
