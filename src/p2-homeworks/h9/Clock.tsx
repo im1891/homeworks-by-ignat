a00ac591a7fd289e0d115f0cc2cabfb72407e243
Igor Muratov <im1891@gmail.com>
2022-10-19 14:40:56 +0300

    Completed HW9.

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -8,43 +8,43 @@ function Clock() {
 
     const stop = () => {
         // stop
+        clearInterval(timerId)
+
     }
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
+        const id: number = +setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() // fix with date
+    const stringDate = date?.toLocaleDateString()// fix with date
 
     return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
+        <div style={{height: '65px'}}>
+            <div style={{display: 'inline-block', minHeight: '22px'}} onMouseEnter={onMouseEnter}
+                 onMouseLeave={onMouseLeave}>
                 {stringTime}
             </div>
-
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            <div style={{minHeight: '22px'}}>
+                {show && stringDate}
+            </div>
+            <div style={{display: 'flex'}}>
+                <SuperButton onClick={start}>start</SuperButton>
+                <SuperButton onClick={stop}>stop</SuperButton>
+            </div>
         </div>
     )
 }
